27106d045982453fe3cf436ff5844cd1387c74f8
Alan Wills <alawills@googlemail.com>
2018-07-15 23:54:57 +0100

    Working on profiling and converter

--- RealTalkProfiling/Program.cs
+++ RealTalkProfiling/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RealTalkEngine.StorySystem;
+using System;
+using System.Diagnostics;
+using Twinary.StorySystem;
 
 namespace RealTalkProfiling
 {
@@ -6,7 +9,31 @@ namespace RealTalkProfiling
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const int iterationCount = 10000;
+            const string jsonPath = @"C:\Repos\Dispatcher\RealTalkEngine\Resources\Json Stories\FragmentsOfSkullMaster.json";
+            const string binPath = @"C:\Repos\Dispatcher\RealTalkEngine\Resources\Binary Stories\FragmentsOfSkullMaster.data";
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            for (int i = 0; i < iterationCount; ++i)
+            {
+                TwineStory twineStory = TwineStory.Load(jsonPath);
+                Story story = Story.Load(twineStory);
+            }
+            stopwatch.Stop();
+
+            Console.WriteLine("Json total milliseconds for " + iterationCount + " iterations: " + stopwatch.ElapsedMilliseconds);
+            Console.WriteLine("Json average milliseconds: " + (stopwatch.ElapsedMilliseconds / (float)iterationCount));
+
+            stopwatch = Stopwatch.StartNew();
+            for (int i = 0; i < iterationCount; ++i)
+            {
+                Story story = Story.Load(binPath);
+            }
+            stopwatch.Stop();
+
+            Console.WriteLine("Bin total milliseconds for " + iterationCount + " iterations: " + stopwatch.ElapsedMilliseconds);
+            Console.WriteLine("Bin average milliseconds: " + (stopwatch.ElapsedMilliseconds / (float)iterationCount));
+            //Console.ReadKey();
         }
     }
 }
---
+++ RealTalkStoryFormatter/Program.cs
@@ -0,0 +1,40 @@
+﻿using RealTalkEngine.StorySystem;
+using System;
+using System.IO;
+using Twinary.StorySystem;
+
+namespace RealTalkStoryFormatter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool completed = false;
+            while (!completed)
+            {
+                Console.WriteLine("Input story file...");
+
+                string input = Console.ReadLine();
+                if (input == "q" || input == "Q")
+                {
+                    completed = true;
+                }
+                else if (File.Exists(input))
+                {
+                    // Process story file
+                    TwineStory twineStory = TwineStory.Load(input);
+                    Story story = Story.Load(twineStory);
+
+                    string outputPath = Path.ChangeExtension(input, "data");
+                    story.Save(outputPath, true);
+
+                    Console.WriteLine("Story saved to " + outputPath);
+                }
+                else
+                {
+                    Console.WriteLine("Unrecognized input...");
+                }
+            }
+        }
+    }
+}
